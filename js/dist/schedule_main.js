7c645998bc9c819422c216d01887336eacf6d6cf
Mark Theunissen <mark.theunissen@gmail.com>
2013-04-27 14:57:47 -0500

    Adding filters

--- js/dist/schedule_main.js
+++ js/dist/schedule_main.js
@@ -30752,7 +30752,10 @@ Ember
 })();
 
 ;(function ($) {
-  var App = Ember.Application.create({LOG_TRANSITIONS: true});
+  var App = Ember.Application.create({
+    LOG_TRANSITIONS: true,
+    rootElement: '#ember-container'
+  });
   window.App = App;
 
   App.SessionModel = Ember.Object.extend({
@@ -30768,7 +30771,25 @@ Ember
 
   App.ScheduleModel = Ember.Object.extend({
     allSessions: [],
-    filteredSessions: [],
+    allTracks: [],
+    allDifficulties: [],
+    track: null,
+    difficulty: null,
+
+    visibleSessions: function() {
+      var track = this.get('track');
+      var difficulty = this.get('difficulty');
+
+      return this.get('allSessions').filter(function(item, index, enumerable) {
+        if (track && item.get('track') !== track) {
+          return false;
+        }
+        if (difficulty && item.get('difficulty') !== difficulty) {
+          return false;
+        }
+        return true;
+      });
+    }.property('allSessions', 'track', 'difficulty'),
 
     resetScheduleInfo: function() {
       var self = this;
@@ -30779,9 +30800,15 @@ Ember
           // We have to add 'false' to the end of our Jekyll-produced JSON otherwise
           // we have a dangling comma. So strip out the false.
           data = data.slice(0, data.length-1);
+          var allTracks = [null];
+          var allDifficulties = [null];
           self.set('allSessions', data.map(function(session) {
+            allTracks.push(session.track);
+            allDifficulties.push(session.difficulty);
             return App.SessionModel.create(session);
           }));
+          self.set('allTracks', allTracks.uniq().toArray());
+          self.set('allDifficulties', allDifficulties.uniq().toArray());
         }
       });
 
@@ -30796,7 +30823,6 @@ Ember
   });
 
   App.ScheduleController = Ember.ObjectController.extend({
-
   });
 
   App.ScheduleRoute = Ember.Route.extend({
