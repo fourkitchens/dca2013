1ad5dc1932af433e0ce59b95594d0631b21eed53
Chris Ruppel <chris.ruppel@gmail.com>
2013-04-27 18:26:04 -0500

    Updating FAQ JS to actually work.

--- _js/faq.js
+++ _js/faq.js
@@ -6,21 +6,22 @@
 
     // Fetch FAQs once to save cycles
     $('.faq').each(function(){
+      // Tag it with a unique class
+      $(this).addClass('faq-'+i);
 
       // collect data from markup
       faqs[i] = {
-        q: $(this).find('.question').html(),
-        a: $(this).find('.answer').html(),
+        q: $(this).find('.question').text(),
+        a: $(this).find('.answer').text(),
         id: i++
       };
-
     });
 
     // insert search box
     $('.faqs').prepend('<input id="magic-search" type="text" placeholder="type to search">');
     $('#magic-search').focus();
 
-    // Search code. Does simple searching on each key press,
+    // Search code as user types. Does simple searching on each key press,
     // then shows/hides individual FAQs
     $('#magic-search').keyup(function(){
 
@@ -29,7 +30,7 @@
           test = '';
 
       // Loop through data and see which ones match
-      for(var x=0; x <= $(faqs).length; x++){
+      for(var x=0; x <= $(faqs).length-1; x++){
 
         // Moar vars!!!
         var thisFaq = faqs[x];
@@ -42,10 +43,10 @@
 
         // Filter this entry, then show/hide
         if ((question.indexOf(val)) >= 0 || (answer.indexOf(val) >= 0)) {
-          $('.views-row-'+(x+1)).show('fast');
+          $('.faq-'+(x)).show('fast');
         }
         else {
-          $('.views-row-'+(x+1)).hide('fast');
+          $('.faq-'+(x)).hide('fast');
         }
 
       }
